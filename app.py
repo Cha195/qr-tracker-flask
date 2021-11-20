2ded0d43041adff82e97bf2739b65d4c50119f1e
Sricharan Ramesh <charan1952001@gmail.com>
2021-11-20 18:51:34 +0530

    feat: add data to clients and users

--- app.py
+++ app.py
@@ -2,8 +2,9 @@
 import firebase_admin
 import pyrebase
 import json
-from firebase_admin import credentials, auth
+from firebase_admin import credentials, auth, firestore
 from flask import Flask, request
+import sys
 
 #App configuration
 app = Flask(__name__)
@@ -12,6 +13,9 @@ app = Flask(__name__)
 cred = credentials.Certificate('fbAdminConfig.json')
 firebase = firebase_admin.initialize_app(cred)
 pb = pyrebase.initialize_app(json.load(open('fbConfig.json')))
+db = firestore.client()
+client_ref = db.collection('Clients')
+user_ref = db.collection('users')
 
 #Data source
 users = [{'uid': 1, 'name': 'Noah Schairer'}]
@@ -25,17 +29,32 @@ def userinfo():
 @app.route('/api/signup')
 def signup():
   email = request.form.get('email')
+  name = request.form.get('name')
+  phone = request.form.get('phone')
   password = request.form.get('password')
-  if email is None or password is None:
-    return {'message': 'Error missing email or password'},400
+  if email is None or password is None or phone is None or name is None:
+    return {'message': 'Error missing email or password'}, 400
   try:
     user = auth.create_user(
       email=email,
       password=password
     )
-    return {'message': f'Successfully created user {user.uid}'},200
-  except:
-    return {'message': 'Error creating user'},400
+    user_ref.document(user.uid).set({
+      "Name": name,
+      "Phone": phone,
+      "Email": email
+    })
+    new_client_ref = client_ref.document()
+    new_client_ref.set({"userId": user.uid})
+    guardians = new_client_ref.collection('guardians')
+    guardians.document().set({
+      "Name": name,
+      "Phone": phone,
+      "Email": email
+    })
+    return {'message': f'Successfully created user {user.uid}'}, 200
+  except Exception as e:
+    return {'message': f'{e}'},400
 
 #Api route to get a new token for a valid user
 @app.route('/api/token')
