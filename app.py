e30bfd84e2cddaa935d886ef54a9bdb26effb265
Sricharan Ramesh <charan1952001@gmail.com>
2021-11-25 15:51:24 +0530

    fix: final fixes

--- app.py
+++ app.py
@@ -6,9 +6,11 @@ from firebase_admin import credentials, auth, firestore
 from flask import Flask, request
 from functools import wraps
 from flask_mail import Mail, Message
+from flask_cors import CORS
 
 #App configuration
 app = Flask(__name__)
+cors = CORS(app)
 
 # initialize mail
 mail_settings = {
@@ -31,23 +33,24 @@ client_ref = db.collection('Clients')
 user_ref = db.collection('users')
 
 def check_token(f):
-    @wraps(f)
-    def wrap(*args,**kwargs):
-        if not request.headers.get('authorization'):
-            return {'message': 'No token provided'}, 400
-        try:
-            user = auth.verify_id_token(request.headers['authorization'])
-            request.user = user
-        except:
-            return {'message':'Invalid token provided.'},400
-        return f(*args, **kwargs)
-    return wrap
+  @wraps(f)
+  def wrap(*args,**kwargs):
+    if not request.headers.get('authorization'):
+      return {'message': 'No token provided'}, 400
+    try:
+      user = auth.verify_id_token(request.headers['authorization'])
+      request.user = user
+    except:
+      return {'message':'Invalid token provided.'},400
+    return f(*args, **kwargs)
+  return wrap
     
 @app.route('/api/poc', methods=['POST'])
 @check_token
 def add_poc():
   try:
-    req_json = request.json
+    print (request.is_json)
+    req_json = request.get_json()
     user = request.user
     userId = user['user_id']
     client_list = list(client_ref.where("userId", "==", userId).stream())
@@ -55,9 +58,9 @@ def add_poc():
     for doc in client_list:
       doc_ids.append(doc.id)
     client_id = doc_ids[0]
-    print(client_id)
     guardian_collec = client_ref.document(client_id).collection('guardians')
     guardians = req_json["guardians"]
+    print(guardians)
     for guardian in guardians:
       guardian_collec.document().set({
         "Name": guardian['name'],
@@ -66,10 +69,11 @@ def add_poc():
       })
     return {'message': f'{user}'}, 200
   except Exception as e:
+    print(e)
     return {'message': f'There was an error in POSTING {e}'}, 400
 
 # Scan QR and send email
-@app.route('/api/scan/<id>')
+@app.route('/api/scan/<id>', methods=['POST'])
 def scanQr(id):
   email = request.form.get('email')
   name = request.form.get('name')
@@ -92,12 +96,12 @@ def scanQr(id):
         conn.send(msg)
 
     
-    return {'message': 'Sent the email'}, 200
+    return {'message': 'Sent the email', 'emailSent': True}, 200
   except Exception as err:
-    return {'message': f'{err}'}, 400
+    return {'message': f'{err}', 'emailSent': False}, 400
 
 #Api route to sign up a new user
-@app.route('/api/signup')
+@app.route('/api/signup', methods=['POST'])
 def signup():
   email = request.form.get('email')
   name = request.form.get('name')
@@ -128,15 +132,23 @@ def signup():
     return {'message': f'{e}'},400
 
 #Api route to get a new token for a valid user
-@app.route('/api/token')
+@app.route('/api/token', methods=['POST'])
 def token():
   email = request.form.get('email')
   password = request.form.get('password')
   try:
     user = pb.auth().sign_in_with_email_and_password(email, password)
     jwt = user['idToken']
-    return {'token': jwt}, 200
-  except:
+    refresh = user['refreshToken']
+    userId = user['localId']
+    client_list = list(client_ref.where("userId", "==", userId).stream())
+    doc_ids = []
+    for doc in client_list:
+      doc_ids.append(doc.id)
+    client_id = doc_ids[0]
+    return { 'access': jwt, 'refresh': refresh, 'user_id': userId, 'client_id': client_id }, 200
+  except Exception as err:
+    print(err)
     return {'message': 'There was an error logging'}, 400
 
 if __name__ == '__main__':
